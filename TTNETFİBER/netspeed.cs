2b295e3476443a45fab5d168a133d8e91a1b5780
Fatin Can Narin <mixsomee@hotmail.com>
2024-05-13 20:47:53 +0300

    son güncellemeler

--- TTNETFİBER/netspeed.cs
+++ TTNETFİBER/netspeed.cs
@@ -1,15 +1,24 @@
+using System;
+using System.Collections.Generic;
 using System.Globalization;
-using System.Net;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using CsvHelper;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using RestSharp;
 using TTNETFİBER;
-
 
 public static class netspeed
 {
     public static void Run()
     {
+        string connectionString = "mongodb://localhost:27017";
+        var mongoclient = new MongoClient(connectionString);
+        var database = mongoclient.GetDatabase("Fiber");
+        var collection = database.GetCollection<BeautifiedModel>("Netspeed");
+
         bool isFirstDepartment = true;
         List<ResultModel> resultList = new List<ResultModel>();
         RestClientOptions options = new RestClientOptions
@@ -18,125 +27,75 @@ public static class netspeed
         };
         RestClient client = new RestClient(options);
 
-        var request =
-            new RestRequest(
-                "/Home/GetAddress", Method.Post);
-
-        // request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        // request.AddParameter("type", "2");
-        // request.AddParameter("id", "1199");
-        // new RestRequest(
-        //     "/Home/GetAddress", Method.Post);
-
+        var request = new RestRequest("/Home/GetAddress", Method.Post);
         request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
         request.AddParameter("type", "3");
         request.AddParameter("id", "6554");
 
         var getMahList = client.ExecuteAsync<ResponseModel>(request).Result;
 
-
-        foreach (var mahItem in getMahList.Data.Data)
+        Parallel.ForEach(getMahList.Data.Data, mahItem =>
         {
-            request =
-                new RestRequest(
-                    "/Home/GetAddress", Method.Post);
-
+           var  request = new RestRequest("/Home/GetAddress", Method.Post);
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
             request.AddParameter("type", "4");
             request.AddParameter("id", mahItem.Code);
 
             var getSokakList = client.ExecuteAsync<ResponseModel>(request).Result;
 
-            foreach (var getSokakItem in getSokakList.Data.Data)
+            Parallel.ForEach(getSokakList.Data.Data, getSokakItem =>
             {
-                request =
-                    new RestRequest(
-                        "/Home/GetAddress", Method.Post);
-
+                var request = new RestRequest("/Home/GetAddress", Method.Post);
                 request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
                 request.AddParameter("type", "5");
                 request.AddParameter("id", getSokakItem.Code);
 
                 var getBinaList = client.ExecuteAsync<ResponseModel>(request).Result;
 
-
-                foreach (var BinaItem in getBinaList.Data.Data)
+                Parallel.ForEach(getBinaList.Data.Data, BinaItem =>
                 {
-                    request =
-                        new RestRequest(
-                            "/Home/GetAddress", Method.Post);
-
+                   var request = new RestRequest("/Home/GetAddress", Method.Post);
                     request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
                     request.AddParameter("type", "6");
                     request.AddParameter("id", BinaItem.Code);
 
                     var getDaire = client.ExecuteAsync<ResponseModel>(request).Result;
 
-                    foreach (var daireItem in getDaire.Data.Data)
+                    Parallel.ForEach(getDaire.Data.Data, daireItem =>
                     {
-                        request =
-                            new RestRequest(
-                                "/Home/GetInfrastractureQueryResult", Method.Post);
-
+                        var request = new RestRequest("/Home/GetInfrastractureQueryResult", Method.Post);
                         request.AddHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
                         request.AddParameter("searchKey", daireItem.Code);
-
 
                         var getResult = client.ExecuteAsync<string>(request).Result;
 
                         if (!string.IsNullOrEmpty(getResult.Data))
                         {
-                            var ConvertedResult =
-                                Newtonsoft.Json.JsonConvert.DeserializeObject<ResultModel>(getResult.Data);
+                            var ConvertedResult = Newtonsoft.Json.JsonConvert.DeserializeObject<ResultModel>(getResult.Data);
 
-                            var lastResult = new BeautifiedModel("Bolu", "Merkez", "Merkez", mahItem.Name,
-                                mahItem.Code, getSokakItem.Name, getSokakItem.Code, BinaItem.Name,
-                                BinaItem.Code, daireItem.Name, daireItem.Code, ConvertedResult);
+                            var lastResult = new BeautifiedModel("Bolu", "Merkez", "Merkez", mahItem.Name, mahItem.Code,
+                                getSokakItem.Name, getSokakItem.Code, BinaItem.Name, BinaItem.Code, daireItem.Name,
+                                daireItem.Code, ConvertedResult);
                             Console.WriteLine(JsonConvert.SerializeObject(lastResult));
-                            WriteToCsv(lastResult, "sonuc.csv", isFirstDepartment);
+
+                            collection.InsertOne(lastResult);
                             isFirstDepartment = false;
                         }
                         else
                         {
                             ResultModel bos = new ResultModel();
-                            var lastResult = new BeautifiedModel("Bolu", "Merkez", "Merkez", mahItem.Name,
-                                mahItem.Code, getSokakItem.Name, getSokakItem.Code, BinaItem.Name,
-                                BinaItem.Code, daireItem.Name, daireItem.Code, bos);
+                            var lastResult = new BeautifiedModel("Bolu", "Merkez", "Merkez", mahItem.Name, mahItem.Code,
+                                getSokakItem.Name, getSokakItem.Code, BinaItem.Name, BinaItem.Code, daireItem.Name,
+                                daireItem.Code, bos);
                             Console.WriteLine(JsonConvert.SerializeObject(lastResult));
 
-
-                            WriteToCsv(lastResult, "sonuc.csv", isFirstDepartment);
+                            collection.InsertOne(lastResult);
                             isFirstDepartment = false;
                         }
-                    }
-                }
-            }
-        }
-
-        static void WriteToCsv(BeautifiedModel result, string filePath, bool writeHeader)
-        {
-            try
-            {
-                using (var writer = new StreamWriter(filePath, true,System.Text.Encoding.UTF8)) // Append modunu true olarak ayarlayın
-                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-                {
-                   
-                    if (writeHeader)
-                    {
-                        csv.WriteHeader<BeautifiedModel>();
-                        csv.NextRecord();
-                    }
-
-                    csv.WriteRecord(result);
-                    writer.WriteLine();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+                    });
+                });
+            });
+        });
     }
 
     private record BeautifiedModel(
